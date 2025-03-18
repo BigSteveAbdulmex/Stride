da075d952021c759f4e7766ef21a1d89333f2165
Stephen Abdulmumeen <steveabdulmexa@gmail.com>
2025-03-18 22:26:42 +0100

    Linked the onboarding file to the app file

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:stride/features/onboarding/onbarding_screen.dart';
+
 class AppEntryPoint extends StatelessWidget {
   const AppEntryPoint({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return OnboardingScreen();
   }
 }
